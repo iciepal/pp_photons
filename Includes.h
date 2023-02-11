92694cfe0332d30313d5bde448837c199d781744
iciepal <izabela.ciepal@ifj.edu.pl>
2023-02-11 20:58:35 +0100

    commit

---
+++ Includes.h
@@ -0,0 +1,49 @@
+// A million includes for looping DSTs that should get out of the way
+#ifndef __INCLUDES_H__
+#define __INCLUDES_H__
+
+#include "TObject.h"
+#include "TROOT.h"
+#include "TFile.h"
+#include "TDirectory.h"
+#include "TSystemDirectory.h"
+#include "TSystemFile.h"
+
+#include "TString.h"
+#include "TMath.h"
+#include "TLorentzVector.h"
+
+#include "TList.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "THn.h"
+#include "TCutG.h"
+#include "TF1.h"
+
+#include "emcdef.h"
+#include "heventmixer.h"
+
+#include "hloop.h"
+#include "hcategorymanager.h"
+#include "htool.h"
+#include "hphysicsconstants.h"
+#include "hparticletracksorter.h"
+#include "hstartdef.h"
+#include "hstart2raw.h"
+#include "hstart2cal.h"
+#include "hstart2hit.h"
+
+#include "hemcneutralcand.h"
+#include "hemcclustersim.h"
+#include "hemccluster.h"
+
+#include "heventheader.h"
+#include "hparticleevtinfo.h"
+#include "hgeantheader.h"
+#include "hparticlecand.h"
+#include "hparticlecandsim.h"
+#include "hgeantkine.h"
+
+#endif // __INCLUDES_H__
